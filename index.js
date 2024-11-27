951e105c4839ce2ed1a98b975a9e9f13ec4c3c58
BasileLgr <basilelegrelle@gmail.com>
2024-11-28 00:20:54 +0100

    test

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const CLIENT_ID = 'fxxny3g1a5svpmrqphc4nz8suq4etx';
 const CLIENT_SECRET = 'c5iw3gkqipzq2hi90dwh061ktsskrx';
 let ACCESS_TOKEN = '';
 
-app.use(cors());
+// Configurez CORS pour autoriser uniquement votre domaine frontend
+const corsOptions = {
+    origin: 'https://basilelgr.github.io', // Domaine de votre frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Méthodes HTTP autorisées
+    credentials: true, // Si vous utilisez des cookies ou des sessions
+};
+
+// Appliquez les options CORS au middleware
+app.use(cors(corsOptions));
 
 const getAccessToken = async () => {
     const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
